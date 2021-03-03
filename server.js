73d8d8fe61e9eaea35db9769ffbe46d2c8689e5b
Trevor Smith <trevorsmithbanjo@gmail.com>
2021-03-03 17:47:21 -0600

    Deploying One More Time

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // ==== Connect Mongoose ==== //
-mongoose.connect(process.env.MONGODB.URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
