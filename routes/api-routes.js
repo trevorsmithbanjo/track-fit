4c922e46d870f27200c1a2943f0037a8ba9dede6
Trevor Smith <trevorsmithbanjo@gmail.com>
2021-03-03 16:59:30 -0600

    Api Routes Working.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,7 +2,16 @@ const db = require("../models");
 
 module.exports = (app) => {
     app.get("/api/workouts", (req, res) => {
-        db.Workout.find({})
+        // get workouts from db and add total duration
+        db.Workout.aggregate([
+            {
+                $addFields: {
+                    totalDuration: {
+                        $sum: "$exercises.duration"
+                    }
+                }
+            }
+        ])
             .then(dbWorkout => {
                 res.json(dbWorkout);
             })
@@ -11,13 +20,8 @@ module.exports = (app) => {
             })
     })
 
-    //app.post("/api/workouts/", ({ body }, res) => {
     app.post("/api/workouts", (req, res) => {
-        //console.log("/api/workouts/ route");
-        console.log("Inside post");
-        console.log(req.body);
-        //console.log({ body });
-
+        // create workout object without exercises
         db.Workout.create({})
             .then(dbWorkout => {
                 res.json(dbWorkout);
@@ -28,26 +32,25 @@ module.exports = (app) => {
     })
 
     app.put("/api/workouts/:id", (req, res) => {
-        console.log("/api/workouts/:id route");
-        console.log(req.body);
-        console.log(`request id: ${req.params.id}`);
-
+        // find workout object by id and add exercises
         db.Workout.findByIdAndUpdate(req.params.id, {
             $push: {
-                exercises: req.body /*[
-                    {
-                        type: req.body.type,
-                        name: req.body.name,
-                        weight: req.body.weight,
-                        sets: req.body.sets,
-                        reps: req.body.reps,
-                        duration: req.body.duration
-                    }
-                ]*/
+                exercises: req.body
             }
         }, { new: true, runValidators: true })
             .then((dbWorkout) => {
                 console.log(`Promise result dbWorkout: ${dbWorkout}`);
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.json(err);
+            })
+    })
+
+    app.get("/api/workouts/range", (req, res) => {
+        // find last 7 workouts to add to stats page
+        db.Workout.find({}).sort({ _id: -1 }).limit(7)
+            .then(dbWorkout => {
                 res.json(dbWorkout);
             })
             .catch(err => {
